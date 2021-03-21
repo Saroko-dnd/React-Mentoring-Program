b7edd6ede61427807c13e5836f9e4195d136990f
Ihar Saroka <Ihar_Saroka@epam.com>
2021-03-21 19:36:12 +0300

    created 4 'Hello World' components using different methods of creating components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class HelloWorldPureComponent extends React.PureComponent {
+  render() {
+    return <h1>Hello world! (PureComponent)</h1>;
+  }
+};
+
+class HelloWorldComponent extends React.Component {
+  render() {
+    return <h1>Hello world! (Component)</h1>;
+  }
+};
+
+const helloWorldElement = React.createElement('h1', null, 'Hello world! (Element)');
+
+function HelloWorldFunctionalComponent(props) {
+  return <h1>Hello world! (Functional component)</h1>;
+}
+
 ReactDOM.render(
-  /*<React.StrictMode>
-    <App />
-  </React.StrictMode>,*/
-  <h1>Hello world!</h1>,
+  [helloWorldElement, <HelloWorldComponent/>, <HelloWorldPureComponent/>, <HelloWorldFunctionalComponent/>],
   document.getElementById('root')
 );
 
